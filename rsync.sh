64bf0fca206bf1f0e3324cb6d392908f26480c4d
Dan Kopparhed <dan.kopparhed@gmail.com>
2014-08-11 19:15:03 +0200

    Adds first revision of a script intended for syncing log files from live systems, for monitoring stats in near-realtime. Actually it will probably be with a one minute delay.

---
+++ rsync.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+##########################################################################
+ABOUT="Script for syncing many and large log files being updated in realtime."
+# Assumes that the logfiles follow a certain format with date and time etc
+# in the file name.
+# ------------------------------------------------------------------------
+# 2014-08-04 Dan Kopparhed
+##########################################################################
+THISFILE=${0##*/}
+SERVER="TBD"
+RPATH="./logfiles"
+LPATH="./"
+args=("$@")
+##########################################################################
+f_usage()
+{
+	cat <<TXT
+ $ABOUT
+ Usage: ${THISFILE}
+TXT
+}
+##########################################################################
+
+# Figure out the correct dates (taking the real calendar into account):
+CURRDATE=$(date +%Y%m%d)
+CURRTIME=$(date +%R)
+CURRTS=$(date +%s)
+PREVTS=$(($CURRTS - 86400))
+PREVMIN=$(($CURRTS - 60))
+PREVTIME=$(date -d @$PREVMIN +%R)
+PREVDATE=$(date -d @$PREVTS +%Y%m%d)
+
+echo "Syncing today ($CURRDATE) and yesterday ($PREVDATE)"
+echo "Today: $(date), yesterday: $(date -d @$PREVTS)"
+echo "This time: $CURRTIME, last minute: $PREVTIME"
+
+# rsync -avz -e ssh user@server:/path ./
+# Rsync logs from today and yesterday:
+rsync -av $RPATH/*\_$CURRDATE\_*.csv $LPATH
+rsync -av $RPATH/*\_$PREVDATE\_*.csv $LPATH
+
