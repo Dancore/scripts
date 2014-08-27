4be4d0c43690cb2785603a766b97ade75345f605
Dan Kopparhed <dan.kopparhed@gmail.com>
2014-08-27 15:41:40 +0200

    Improvements.

--- css_realtime_taplat_rsync.pl
+++ css_realtime_taplat_rsync.pl
@@ -1,47 +1,90 @@
 #!/usr/bin/perl
 # Rsync the requested files to the destination
 #
-# css_realtime_taplat_rsync.pl [days]
-#
+# css_realtime_taplat_rsync.pl [days back]
+# 2014-08-20 v1 - Inital creation done.
 #
 ######
 use strict;
 use warnings;
 use POSIX;
+use Cwd qw();
+
 ##Static config
 my @dates;
-my $days_back = $ARGV[0] || 2;
-my $taplat_folder = $ARGV[1] || "/opt/omex/tom/tom/omn/cur/dat/";
+my $days_back = $ARGV[0] || 1;
+
 ##
 ##User config
-my $gateways= "tx0600,tx0601,tx0602,tx0602"; ## Colon separeade list
+my $taplat_folder = "/opt/omex/tom/tom/omn/cur/dat/"; #Remote folders where taplat is stored
+my $taplat_processing_folder = "taplat"; #localt processing folder.
+my $logfile_folder = "logfiles";
+my $gateways= "UK03TOMTVBE31,UK03TOMTVGW06,UK03TOMTVGW07"; ## Colon separeade list "UK03TOMTVBE31,UK03TOMTVGW06,UK03TOMTVGW07"
 ##
 
-calculate_dates();
-run_rsync();
-
-sub calculate_dates
-{
-	for (my $days = 0; $days <= $days_back-1;$days++)
+#Loggstuff
+my $directory = Cwd::cwd();
+my $date = strftime "%Y%m%d",(localtime(time()));
+unless (-d $logfile_folder)
 	{
+		mkdir $logfile_folder;
+		print "Creating directory $logfile_folder\n";
+		
+	}
+my $logfile = $directory."/".$logfile_folder."/"."rsync_".$date.".log";
+open STDOUT, ">>", "$logfile" or die "$0: open: $!";
+open STDERR, ">>&STDOUT"        or die "$0: dup: $!";
+
+
+
+##Main Code
+#Create taplat folder if not existing
+unless (-d $taplat_processing_folder)
+	{
+		mkdir $taplat_processing_folder;
+		print "Creating directory $taplat_processing_folder\n";
+		
+	}
+
+calculate_dates()==0 || die "Filed in Calculate_dates $! \n";
+run_rsync()==0 || die "Filed in run_rsync $! \n";;
+
+exit 0;
+
+#SCRIPT end
+
+sub calculate_dates{
+
+		for (my $days = 0; $days <= $days_back;$days++)
+		{
 		my $timeelement = strftime "%Y%m%d",(localtime(time() - $days*24*60*60));
 		print $timeelement."\n";
 		push (@dates,$timeelement);
-	}
+		}
+		
 }
 
-sub run_rsync
-{
+sub run_rsync{
 	my @gateway_array = split (',',$gateways);
+	
+	
 	foreach my $gate(@gateway_array)
 	{
 		foreach my $date (@dates)
-		{
-			my $command = "rsync -avz ".$gate.":".$taplat_folder.$date."*.csv ./";
-			print $command ."\n";
-			#system($command) == 0 || die "Failed to run rsync command: $command $! \n";
-		}
-	}
-}
+		{		
 
-1;
+		my $command = "rsync -avz ".$gate.":".$taplat_folder."*".$date."*.csv ./$taplat_processing_folder";
+		print $command ."\n";
+		system($command);
+		
+		}
+		}
+		
+		
+		
+			
+	}
+	
+	
+	
+	
